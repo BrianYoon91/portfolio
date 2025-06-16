a07f21a2a5f6e30017553e1d9a18d4d21bbf8800
Brian Yoon <yoon.brian91@gmail.com>
2025-06-16 13:17:04 -0400

    adding google analytics

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -13,9 +13,9 @@ export const Projects = () => {
 				const { name, description, github, live, image } = project;
 				return (
 					<>
-						<div className="m-5 h-full flex justify-center items-center">
+						<div className="m-5 min-h-72 flex justify-center items-center">
 							<div
-								className="rounded border-2 border-pink-200 p-3 m-3
+								className="rounded border-2 border-pink-200 p-3 m-3 flex flex-col gap-5
 							"
 							>
 								<Image
@@ -26,49 +26,51 @@ export const Projects = () => {
 									}}
 									preview={false}
 								/>
-								<h2 className="text-center text-[#eebbc3] text-xl">{name}</h2>
-								<p>{description}</p>
-								<div className="m-5 grid grid-cols-4 gap-2">
-									<a
-										className="col-span-2 w-auto"
-										href={github}
-										target="_blank"
-										rel="noreferrer"
-									>
-										<Button
-											style={{
-												color: isGithubHovered ? "#eebbc3" : "#fffffe",
-												borderColor: isGithubHovered ? "#eebbc3" : "#fffffe",
-												backgroundColor: "#232946",
-												width: "100%",
-											}}
-											icon={<GithubOutlined />}
-											onMouseEnter={() => setIsGithubHovered(true)}
-											onMouseLeave={() => setIsGithubHovered(false)}
+								<div>
+									<h2 className="text-center text-[#eebbc3] text-xl">{name}</h2>
+									<p>{description}</p>
+									<div className="m-5 grid grid-cols-4 gap-2">
+										<a
+											className="col-span-2 w-auto"
+											href={github}
+											target="_blank"
+											rel="noreferrer"
 										>
-											GitHub
-										</Button>
-									</a>
-									<a
-										className="col-span-2 w-auto"
-										href={live}
-										target="_blank"
-										rel="noreferrer"
-									>
-										<Button
-											style={{
-												backgroundColor: "#232946",
-												color: isLiveHovered ? "#eebbc3" : "#fffffe",
-												borderColor: isLiveHovered ? "#eebbc3" : "#fffffe",
-												width: "100%",
-											}}
-											icon={<GlobalOutlined />}
-											onMouseEnter={() => setIsLiveHovered(true)}
-											onMouseLeave={() => setIsLiveHovered(false)}
+											<Button
+												style={{
+													color: isGithubHovered ? "#eebbc3" : "#fffffe",
+													borderColor: isGithubHovered ? "#eebbc3" : "#fffffe",
+													backgroundColor: "#232946",
+													width: "100%",
+												}}
+												icon={<GithubOutlined />}
+												onMouseEnter={() => setIsGithubHovered(true)}
+												onMouseLeave={() => setIsGithubHovered(false)}
+											>
+												GitHub
+											</Button>
+										</a>
+										<a
+											className="col-span-2 w-auto"
+											href={live}
+											target="_blank"
+											rel="noreferrer"
 										>
-											Live
-										</Button>
-									</a>
+											<Button
+												style={{
+													backgroundColor: "#232946",
+													color: isLiveHovered ? "#eebbc3" : "#fffffe",
+													borderColor: isLiveHovered ? "#eebbc3" : "#fffffe",
+													width: "100%",
+												}}
+												icon={<GlobalOutlined />}
+												onMouseEnter={() => setIsLiveHovered(true)}
+												onMouseLeave={() => setIsLiveHovered(false)}
+											>
+												Live
+											</Button>
+										</a>
+									</div>
 								</div>
 							</div>
 						</div>
